94840ebf5672676a441cc381921de9ee0dff0338
Naimy441 <naimywatermelon@gmail.com>
2025-04-17 19:48:23 -0400

    Added edge config for questions

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,29 +1,12 @@
-"use client"
+import { get } from '@vercel/edge-config';
+import Quiz from "../ui/quiz";
 
-import { useState } from "react";
-import { ProgressBar } from "../ui/progress-bar";
-import { useRouter } from "next/navigation";
-import Question from "../ui/question";
-import questions from "../questions";
+export default async function Page() {
+    const questions = await get('test');
 
-export default function Page() {
-    const router = useRouter();
-    const [index, setIndex] = useState(0);
-
-    const handleAnswer = (answer: string) => {
-        console.log("User answered:", answer);
-        if (index + 1 < questions.length) {
-            setIndex(index + 1);
-        } else {
-            console.log("Quiz complete!");
-            router.push("/end");
-        }
-    };
-
-    return (
-        <div>
-            <ProgressBar percentage={100 * (index + 1) / questions.length} />
-            <Question index={index + 1} question={questions[index]['question']} answers={questions[index]['answers']} onAnswer={(answer) => handleAnswer(answer)} />
-        </div>
-    );
+    if (!questions || !Array.isArray(questions)) {
+        throw new Error("Unable to load questions.");
+    }
+    
+    return <Quiz questions={questions} />;
 }
---
+++ app/ui/quiz.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { Suspense, useState } from "react";
+import { ProgressBar } from "./progress-bar";
+import { CardSkeleton } from "./skeletons";
+import Question from "./question";
+
+export default function Quiz({questions}:{questions: any[]}) {
+    const router = useRouter();
+    const [index, setIndex] = useState(0);
+
+    const handleAnswer = (answer: string) => {
+        console.log("User answered:", answer);
+        if (index + 1 < questions.length) {
+            setIndex(index + 1);
+        } else {
+            console.log("Quiz complete!");
+            router.push("/end");
+        }
+    };
+
+    return (
+        <div>
+            <Suspense fallback={<CardSkeleton />}>
+                <ProgressBar percentage={100 * (index + 1) / questions.length} />
+                <Question index={index + 1} question={questions[index]['question']} answers={questions[index]['answers']} onAnswer={(answer) => handleAnswer(answer)} />
+            </Suspense>
+        </div>
+    );
+}
